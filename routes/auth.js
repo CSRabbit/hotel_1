e83644246158601c5367e97a0d192673cce27d7c
Jurarut Subcheevaanan <cs_rabbit@Juraruts-MacBook-Pro.local>
2021-02-04 15:45:02 +0700

    register page hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const brcrypt = require('brcrypt');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../model/user');
 
