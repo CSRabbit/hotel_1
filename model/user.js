bd1aebcc6f5299001a609c3c5bf2730b5c84994a
Jurarut Subcheevaanan <cs_rabbit@Juraruts-MacBook-Pro.local>
2021-02-04 16:39:03 +0700

    fix form register

--- model/user.js
+++ model/user.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  name: String,
+  
   username: {
     type: String,
     unique: true
   },
-  password: String
+  password: String,
+  name: String,
+  phone: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  citizenID: {
+    type: String,
+    unique: true
+  },
 })
 
 const UserModel = mongoose.model('User', userSchema)
